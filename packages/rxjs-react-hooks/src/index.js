02ce0afbdcdf5470496841f03f3906fbe9ffcfec
wenjun <laojiaodaoniu@126.com>
2022-07-04 11:19:53 +0800

    chore[rrh]: add react router

--- packages/rxjs-react-hooks/src/index.js
+++ packages/rxjs-react-hooks/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+
+const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
